b63d52c5f8b8ee6151e708ae60f0c1bc69552911
Luis Baquera <luis.baquera@ucr.edu>
2024-07-27 03:13:41 -0700

    Load new SECRET_KEY from .env

--- user_management/user_management/settings.py
+++ user_management/user_management/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t&u0zsdm=53clshho%+q4=$cs_(v+u=6ltm%jl7z!ojj&hu22q'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
