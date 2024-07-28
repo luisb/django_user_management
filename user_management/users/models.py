e33e026973e3c0aee213b23a616487a41dc56663
Luis Baquera <luis.baquera@ucr.edu>
2024-07-28 11:09:19 -0700

    Edit Profile--edit user and profile

--- user_management/users/models.py
+++ user_management/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -10,3 +11,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.avatar.path)
+
+        if img.height > 100 or img.width > 100:
+            new_img = (100, 100)
+            img.thumbnail(new_img)
+            img.save(self.avatar.path)
